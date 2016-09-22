e644943b9f0e79310bbabb936745c289bfa8dd29
JV-BugMaker <794241063@qq.com>
2016-09-23 01:29:25 +0800

    [*]修改sass样式

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -18,15 +18,23 @@
     </ul>
     <div class="log-area">
         <ul class="login">
-            <li>
-                <a href="remind_wrap"></a>
-                
+            <li class="remind_warp">
+                <i class="msg_remind" style="display: none;"></i>
+                <a target="_blank" href="/u/1041298/notices"><i class="icon-notifi"></i></a>
             </li>
-            <li>
-                <a href="my_message"></a>
+            <li class="my_message">
+                <a href="/u/1041298/messages" title="我的消息" target="_blank">
+                    <span class="msg_icon" style="display: none;"></span>
+                    <i class="icon-mail"></i>
+                    <span style="display: none;">我的消息</span>
+                </a>
             </li>
-            <li>
-                <a href="user_center"></a>
+            <li class="set_btn user-card-box">
+                <a id="header-avator" class="user-card-item js-header-avator" action-type="my_menu" href="/u/1041298" target="_self">
+                    <img width="40" height="40" src="http://img.mukewang.com/5458476500014e6402200220-100-100.jpg">
+                    <i class="myspace_remind" style="display: none;"></i>
+                    <span style="display: none;">动态提醒</span>
+                </a>
             </li>
         </ul>
     </div>
